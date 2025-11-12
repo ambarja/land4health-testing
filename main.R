3515beb44b0b80f6ac870d676577b11dc415bfa3
Antony Barja <antony.barja8@gmail.com>
2025-11-12 12:20:28 -0500

    Remove rgee library import from main.R
    Removed the library(rgee) line from the script.

--- main.R
+++ main.R
@@ -11,7 +11,6 @@ py_run_string(paste(
   "ee.Initialize(cred, project=os.getenv('GEE_PROJECT_ID'))",
   sep="\n"
 ))
-library(rgee)
 # Downloading the adminstration limits of Loreto provinces
 provinces_loreto <- get_provinces(show_progress = FALSE) |>
   subset(nombdep == "LORETO")
