b19779801f775d073dd0d18ebe685081247b5185
Antony Barja <antony.barja8@gmail.com>
2025-11-14 20:10:59 -0500

    Update main.R

--- main.R
+++ main.R
@@ -10,3 +10,12 @@ py_run_string(paste(
   "ee.Initialize(cred, project=os.getenv('GEE_PROJECT_ID'))",
   sep="\n"
 ))
+library(geoidep)
+# Downloading the adminstration limits of Loreto provinces
+provinces_loreto <- get_provinces(show_progress = FALSE) |>
+  subset(nombdep == "LORETO")
+
+# Run forest loss calculation
+result <- provinces_loreto |>
+  l4h_forest_loss(from = '2005-01-01', to = '2020-01-01', sf = TRUE)
+head(result)
